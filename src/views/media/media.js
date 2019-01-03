9c0ee1fd79e836676cc9928fda0ea8a47dca3434
Eric Dunham <eric.dunham44@gmail.com>
2019-01-03 16:46:10 -0500

    Made changes to the media page and how the video is displayed

--- src/views/media/media.js
+++ src/views/media/media.js
@@ -5,7 +5,9 @@ new Vue({
     data: {
         media: null,
         selectedFolder: null,
-        selectedFile: null
+        selectedFile: null,
+        mediaPath: null,
+        searchPath: "C:\\Users\\edunham4\\Downloads"
     },
     methods: {
         _getMedia(path) {
@@ -47,139 +49,45 @@ new Vue({
         closeModal() {
             this.selectedFile = null;
         },
-        playClick() {
-
-        },
-        playSeek(value) {
-            
-        },
-        pauseClick() {
-
-        },
-        seek(value) {
-
-        },
-        onSeek(value) {
-
-        },
-        fullscreenClick() {
-
-        },
-        muteClick() {
-            
-        }
     },
     beforeMount() {
-        this._getMedia("C:\\Users\\edunham4\\Downloads");
+        this._getMedia(this.searchPath);
     }
-})
+});
 
-/*
-function init() {
-    var mediaInput = document.getElementById("inputPath");
-    mediaInput.value = self.mediaPath;
-
-    self.media = null;
-    self.parents = [];
-    self.elements = {};
-
-    mediaInput.addEventListener("keydown", function (e) {
-        if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-            getMedia(e.target.value);
-        }
-    });
-
-    getMedia(self.mediaPath);
+/*function playClick() {
+    if (!vid.paused) { return pauseClick(); }
+    vid.play();
 }
 
-function getMedia(path) {
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var mediafiles = null;
-            
-            try {
-                mediafiles = JSON.parse(this.responseText);
-            } catch(err) {
-                toastr("No media found", 3000);
-                return;
-            }
-
-            self.media = JSON.parse(mediafiles);
-            self.parents = [];
-            self.elements = {};
-
-            if (!self.media.Name) { self.media.Name = self.media.Path.replace("\\",""); }
-            self.elements[self.media.Name] = self.media;
-
-            addParent(self.media);
-            createMedia(self.media);
-        }
-    };
-
-    xhttp.open("POST", "/media/findMedia", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(path);
+function playingSeek(value) {
+    var interval = setInterval(function () {
+        seeker.value = vid.currentTime;
+        if (vid.paused || vid.ended) { clearInterval(interval); }
+    }, 500);
 }
 
-function addParent(branch) {
-    branch.Branches.forEach(b => {
-        b['parent'] = branch
-        addParent(b);
-    });
+function pauseClick() {
+    vid.pause();
 }
 
-//Change name to path 
-
-function createMedia(data) {
-    var parent = document.getElementById("file-browser");
-    parent.innerHTML = "";
-
-    if (!data.Name) { data.Name = data.Path.replace("\\",""); }
-
-    if(data['parent']) {
-        var html = `
-            <div class="c_overlay">..</div>
-            <i class="material-icons icon" id="back" onclick="createMedia(self.elements['${data.parent.Name}'])">folder</i>
-        `;
-
-        createElement("div", { "class": "media-file", "dir-path": ".." }, html, parent, false);
-    }
-
-    data.Branches.forEach(folder => {
-        html = `
-                <div class="c_overlay">${folder.Name}</div>
-                <i class="material-icons icon" id="${folder.Name}" onclick="createMedia(self.elements[this.id])">folder</i>
-                `;
-
-        self.elements[folder.Name.replace("\\","")] = folder;
-
-        var el = createElement("div", { "class": "media-file", "dir-path": folder.Path }, html, parent, false);
-    });
-
-
-    if (!data.Nodes || !data.Nodes.length) {
-        toastr("No Nodes Found", 3000);
-        return;
-    }
-
-    data.Nodes.forEach(file => {
-        html = `
-                <div class="c_overlay">${file.Name}</div>
-                <i class="material-icons icon" onclick="playMedia('${file.Video}')">movie</i>
-            `;
-
-        var el = createElement("div", { "class": "media-file", "file-name": file }, html, parent, false);
-    });
+function seek(value) {
+    vid.currentTime = value;
 }
 
-function closeModel() {
-    var parent = document.getElementById("video-modal");
-    parent.remove();
+function onSeek(value) {
+    seeker.value = value;
 }
 
-//elem.addEventListener("touchstart", defaultPrevent);
-//elem.addEventListener("touchmove" , defaultPrevent);
+function volumeChange(value) {
+    vid.volume = Number(value);
+}
 
-init();*/
+function fullscreenClick() {
+    vid.webkitRequestFullScreen();
+}
+
+function muteClick() {
+    vid.volume = 0;
+    volume.value = 0;
+}*/
--- src/views/media/video.js
+++ src/views/media/video.js
@@ -1,4 +1,4 @@
-var vid = null;
+/*var vid = null;
 var seeker = null;
 var volume = null;
 var play = null;
@@ -50,40 +50,4 @@ function videoInit() { // Add a document.onload
     
     seeker.max = String(Math.ceil(vid.duration))
 }
-
-function playClick() {
-    if (!vid.paused) { return pauseClick(); }
-    vid.play();
-}
-
-function playingSeek(value) {
-    var interval = setInterval(function () {
-        seeker.value = vid.currentTime;
-        if (vid.paused || vid.ended) { clearInterval(interval); }
-    }, 500);
-}
-
-function pauseClick() {
-    vid.pause();
-}
-
-function seek(value) {
-    vid.currentTime = value;
-}
-
-function onSeek(value) {
-    seeker.value = value;
-}
-
-function volumeChange(value) {
-    vid.volume = Number(value);
-}
-
-function fullscreenClick() {
-    vid.webkitRequestFullScreen();
-}
-
-function muteClick() {
-    vid.volume = 0;
-    volume.value = 0;
-}
+*/
